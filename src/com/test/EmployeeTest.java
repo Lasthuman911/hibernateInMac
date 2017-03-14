b4a49aa58c8ee5f590f00d37550b0ac0b2f8c46c
wzm <785685931@qq.com>
2017-03-14 15:03:23 +0800

    load - get   openSession() --- getCurrentSession

--- src/com/test/EmployeeTest.java
+++ src/com/test/EmployeeTest.java
@@ -14,10 +14,80 @@ import java.util.Date;
  */
 public class EmployeeTest {
     public static void main(String[] args) {
-        addEmplyee();
+
+
+
+        //loadAndGetTest();
+
+
+        //getCurrentSessionTest();
+
+        //openSessionTest();
+
+        //addEmployee2();
+        //addEmplyee();
         //updateEmplyee();
         //deleteEmplyee();
 
+    }
+
+    /**
+     * 观察sql的输出方式，比较load和get 方法的区别
+     */
+    private static void loadAndGetTest() {
+        Session session = SessionFactoryUtil.getSessionFactory().openSession();
+        Employee employee = session.load(Employee.class,1);
+        System.out.println(employee.getName());
+
+        Employee employee1 = session.load(Employee.class,1);
+        System.out.println(employee1.getEmail());
+
+        Employee employee2 = session.load(Employee.class,3);
+        System.out.println(employee2.getHireDate());
+
+        Employee employee3 = session.get(Employee.class,4);
+
+        Employee employee4 = session.get(Employee.class,4);
+    }
+
+    /**
+     * 每次都返回同一个会话
+     */
+    private static void getCurrentSessionTest() {
+        Session session1 = SessionFactoryUtil.getSessionFactory().getCurrentSession();
+        Session session2 = SessionFactoryUtil.getSessionFactory().getCurrentSession();
+        System.out.println(session1.hashCode() + " " +session2.hashCode());
+    }
+
+    /**
+     * openSession() 每次都会创建一个新的会话
+     */
+    private static void openSessionTest() {
+        Session session1 = SessionFactoryUtil.getSessionFactory().openSession();
+        Session session2 = SessionFactoryUtil.getSessionFactory().openSession();
+        System.out.println(session1.hashCode() + " " + session2.hashCode());
+    }
+
+    /**
+     * 用了单例模式，一个应用程序只生成一个SessionFactory
+     */
+    private static void addEmployee2() {
+        Session session = SessionFactoryUtil.getSessionFactory().openSession();
+        try {
+            Transaction transaction = session.beginTransaction();
+            Employee employee = new Employee();
+            employee.setName("wzm");
+            employee.setEmail("wzm@163.com");
+            employee.setHireDate(new Date());
+            session.save(employee);
+            transaction.commit();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
     }
 
     private static void deleteEmplyee() {
@@ -65,5 +135,6 @@ public class EmployeeTest {
         employee.setHireDate(new Date());
         session.save(employee);
         transaction.commit();
+        session.close();
     }
 }
