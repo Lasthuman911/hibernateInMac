9bb097b27e8bd7f7aede1a4369f330b4b6c3339f
lszhen911 <1078169344@qq.com>
2017-03-24 23:28:54 +0800

    hibernate In Action

---
+++ Spring/src/ch1/CDPlayerConfig.java
@@ -0,0 +1,12 @@
+package ch1;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by lszhen on 2017/3/24.
+ */
+@Configuration
+@ComponentScan
+public class CDPlayerConfig {
+}
